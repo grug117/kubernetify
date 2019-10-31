afe351789ad711bf895df8f3cbfe41d88a0e6418
Greg Ford <greg.ford@infinityworks.com>
2019-10-31 22:56:41 +0000

    added script so kubernetify can be used on command line

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
   test_suite='nose.collector',
   tests_require=['nose'],
   install_requires=[],
+  scripts = ['bin/kubernetify'],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
