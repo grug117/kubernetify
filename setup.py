2e19792d68b59b386abe74e0ab37aee26cb3d1e8
Greg Ford <greg.ford@infinityworks.com>
2019-10-31 22:58:39 +0000

    updated release verison to 1 from 0.3

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open('README.rst') as file:
 setup(
   name = 'kubernetify',         # How you named your package folder (MyLib)
   packages=find_namespace_packages(),   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'takes a string and kubernetifies it',   # Give a short description about your library
   author = 'Greg Ford',                   # Type in your name
   author_email = 'gregford117@live.com',      # Type in your E-Mail
   url = 'https://github.com/gregford117/kubernetify',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/gregford117/kubernetify/archive/0.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/gregford117/kubernetify/archive/1.tar.gz',    # I explain this later on
   keywords = ['KUBERNETES', 'KUBERNETIFY'],   # Keywords that define your package best
   test_suite='nose.collector',
   tests_require=['nose'],
