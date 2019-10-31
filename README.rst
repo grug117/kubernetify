79e4d5e34b21adbc9ef382b4764d12593025b2e1
Connor A <connor.jga@gmail.com>
2019-10-31 19:31:59 +0000

    Project description for pip

---
+++ README.rst
@@ -0,0 +1,7 @@
+Kubernetify
+--------
+
+To use (with caution), simply do::
+
+    >>> import kubernetify
+    >>> print kubernetify.kubernetify("string")
--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup
+def readme():
+  with open('README.rst') as file:
+    return file.read()
+
 setup(
   name = 'kubernetify',         # How you named your package folder (MyLib)
   packages = ['kubernetify'],   # Chose the same as "name"
   version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'takes a string and kubernetifies it',   # Give a short description about your library
+  long_description=readme(),
   author = 'Greg Ford',                   # Type in your name
   author_email = 'gregford117@live.com',      # Type in your E-Mail
   url = 'https://github.com/gregford117/kubernetify',   # Provide either the link to your github or to your website
